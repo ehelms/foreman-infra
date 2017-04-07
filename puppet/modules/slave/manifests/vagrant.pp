02e7f277c85b8a14a6a2f1a1a16b669bd57494d5
Eric D. Helms <ericdhelms@gmail.com>
2017-04-07 08:54:17 -0400

    Update Vagrant to latest 1.9.3

--- puppet/modules/slave/manifests/vagrant.pp
+++ puppet/modules/slave/manifests/vagrant.pp
@@ -4,11 +4,11 @@ class slave::vagrant($username, $api_key) {
 
   case $::osfamily {
     'RedHat': {
-      $vagrant_source = 'https://releases.hashicorp.com/vagrant/1.4.2/vagrant_1.4.2_x86_64.rpm'
+      $vagrant_source = 'https://releases.hashicorp.com/vagrant/1.9.3/vagrant_1.9.3_x86_64.rpm'
       $vagrant_provider = 'rpm'
     }
     'Debian': {
-      $vagrant_source = 'https://releases.hashicorp.com/vagrant/1.4.2/vagrant_1.4.2_x86_64.deb'
+      $vagrant_source = 'https://releases.hashicorp.com/vagrant/1.9.3/vagrant_1.9.3_x86_64.deb'
       $vagrant_provider = 'dpkg'
     }
     default: { fail("Unknown osfamily ${::osfamily}") }
